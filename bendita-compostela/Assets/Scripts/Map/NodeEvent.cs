d54dee49ac297ec82e52498ac7fbad9789149bdb
edsonfe92 <ev.fernandez.2019@alumnos.urjc.es>
2023-09-06 22:33:37 +0200

    fixs

--- bendita-compostela/Assets/Scripts/Map/NodeEvent.cs
+++ bendita-compostela/Assets/Scripts/Map/NodeEvent.cs
@@ -7,14 +7,23 @@ public class NodeEvent : MonoBehaviour
     public Node nodeInfo;
     public bool isCompleted = false;
     [SerializeField] private SpriteRenderer sprRenderer;
-    [SerializeField] private BoxCollider2D nodeCollider; 
-    [SerializeField] private Sprite completedNodeSprite;
+    [SerializeField] private BoxCollider2D nodeCollider;     
         
     // TODO remake with delegates
     private void Update() 
     {
-        if(nodeCollider.enabled == false) sprRenderer.color = new Color(0.5660378f,0.5660378f,0.5660378f,1f);        
-        else sprRenderer.color = new Color(1,1,1,1);                   
+        if(nodeCollider.enabled == false)
+        {
+            sprRenderer.color = new Color(0.5660378f,0.5660378f,0.5660378f,1f);        
+            if(isCompleted)
+            {
+                sprRenderer.color = new Color(1,1,1,1);
+            }
+        }            
+        else 
+        {
+            sprRenderer.color = new Color(1,1,1,1);   
+        }
         
     }    
 }
