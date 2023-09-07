896e07b2c10c69907cadac5d29ff45f16d7a526a
ElDiegu <eldieguuu@gmail.com>
2023-09-07 02:16:51 +0200

    Bug fixing

--- bendita-compostela/Assets/Scripts/Database Scripts/ObjectDatabase.cs
+++ bendita-compostela/Assets/Scripts/Database Scripts/ObjectDatabase.cs
@@ -14,8 +14,8 @@ public class SODataBase : MonoBehaviour
         Resources.LoadAll<CondecorationData>("Scriptable Objects/Condecorations").Where(condecoration => !condecoration.special).ToList();
     [field: SerializeField] public static List<PoemData> poems { get; private set; } = Resources.LoadAll<PoemData>("Scriptable Objects/Poems").ToList();
     [field: SerializeField] public static List<ArmorData> chestArmors { get; private set; } =
-        Resources.LoadAll<ArmorData>("Scriptable Objects/Armors").Where(armor => armor.armorType == 0).ToList();
+        Resources.LoadAll<ArmorData>("Scriptable Objects/Cards/Armors").Where(armor => armor.armorType == 0).ToList();
 
     [field: SerializeField] public static List<ArmorData> legArmors { get; private set; } =
-    Resources.LoadAll<ArmorData>("Scriptable Objects/Armors").Where(armor => armor.armorType == 1).ToList();
+    Resources.LoadAll<ArmorData>("Scriptable Objects/Cards/Armors").Where(armor => armor.armorType == 1).ToList();
 }
