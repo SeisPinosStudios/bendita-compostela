fc6f658fcb4f43dbe37adb3665b3f37b21c9ae5e
peloErisado <erisbernales@gmail.com>
2023-11-04 15:07:06 +0100

    Visual changes: lightning added to batte scene
    Global and local lights added to battle background.
    Objects positions now depends on sorting layers.
    Objects sorting layers affects: entities, inventory screens, map and UI.
    Parallax effect added to battle scene: only affects background components and entity.

--- bendita-compostela/Assets/MouseParallaxEffect.cs
+++ bendita-compostela/Assets/MouseParallaxEffect.cs
@@ -7,13 +7,14 @@ public class MouseParallaxEffect : MonoBehaviour
     [SerializeField] private GameObject[] layers;
     [SerializeField] private float MouseSpeedX = 0.05f;
     private Vector3[] OriginalPositions;
+
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
         OriginalPositions = new Vector3[layers.Length];
-
-        for(int i = 0; i<layers.Length; i++)
+        for (int i = 0; i < layers.Length; i++)
         {
             OriginalPositions[i] = layers[i].transform.position;
         }
@@ -22,13 +23,13 @@ public class MouseParallaxEffect : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float x, y;
+        float x;
         x = (Input.mousePosition.x - (Screen.width / 2)) * MouseSpeedX / Screen.width;
- 
 
-        for(int i = 1; i <layers.Length+1; i++)
+
+        for (int i = 1; i < layers.Length + 1; i++)
         {
-            layers[i - 1].transform.position = OriginalPositions[i - 1] + (new Vector3(x, 0f,  0f) * i * ((i - 1) - (layers.Length / 2)));
+            layers[i - 1].transform.position = OriginalPositions[i - 1] + (new Vector3(x, 0f, 0f) * i * ((i - 1) - (layers.Length / 2)));
         }
 
     }
